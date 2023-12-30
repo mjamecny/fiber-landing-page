bb2db813c2456780036e042c38fcdbd764ec1672
mjamecny <jamecny.m@tutanota.com>
2023-12-30 22:27:16 +0100

    add styles for Testimonials section

--- src/Testimonial.jsx
+++ src/Testimonial.jsx
@@ -1,3 +1,64 @@
-export default function Testimonial() {
-  return <div>Testimonial</div>
+import styled from "styled-components"
+
+const StyledTestimonial = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  border: 1px solid var(--color-slate-300);
+  border-radius: 4px;
+  padding: 2.4rem;
+`
+
+const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.6rem;
+`
+
+const Avatar = styled.img``
+
+const UserInfo = styled.div``
+
+const Name = styled.p`
+  color: var(--color-indigo-700);
+  font-weight: 700;
+`
+
+const Revenue = styled.p`
+  font-size: 1.4rem;
+  font-weight: 500;
+  color: var(--color-neutral-950);
+  line-height: 1.2;
+`
+
+const Content = styled.p`
+  color: var(--color-neutral-950);
+`
+
+const Button = styled.button`
+  border: 1px solid var(--color-slate-300);
+  border-radius: 4px;
+  background-color: transparent;
+  color: var(--color-indigo-700);
+  font-weight: 700;
+  padding: 1.6rem 1.6rem;
+  margin-top: auto;
+`
+
+export default function Testimonial({ testimonial }) {
+  const { avatar, name, content, revenue } = testimonial
+
+  return (
+    <StyledTestimonial>
+      <UserContainer>
+        <Avatar src={avatar} alt={`avatar of ${name}`} />
+        <UserInfo>
+          <Name>{name}</Name>
+          <Revenue>${revenue} in revenue</Revenue>
+        </UserInfo>
+      </UserContainer>
+      <Content>{content}</Content>
+      <Button>View {name.split(" ")[0]}'s Portfolio</Button>
+    </StyledTestimonial>
+  )
 }
--- src/Testimonials.jsx
+++ src/Testimonials.jsx
@@ -1,3 +1,58 @@
+import styled from "styled-components"
+
+import Testimonial from "./Testimonial"
+
+const StyledTestimonials = styled.section`
+  padding-bottom: 6.4rem;
+`
+
+const Container = styled.div`
+  padding-inline: 2.4rem;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 2.4rem;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+    padding-inline: 0;
+  }
+`
+
+const testimonials = [
+  {
+    id: 1,
+    avatar: "user-avatar-2.svg",
+    name: "Sarah Andrews",
+    content:
+      "Setting up my portfolio with Fiber took no more than 10 minutes. Since then, my portfolio has attracted a lot of clients and made me more than $100k.",
+    revenue: "100k",
+  },
+  {
+    id: 2,
+    avatar: "user-avatar.svg",
+    name: "Matthew Higgins",
+    content:
+      "I have been getting A LOT of leads ever since I used Fiber's premade templates, now I just need to work on my case studies and I'll be ready to go!",
+    revenue: "20k",
+  },
+  {
+    id: 3,
+    avatar: "user-avatar-3.svg",
+    name: "Janice Dave",
+    content:
+      "I only just started freelancing this year and I have already made more than I ever made in my full-time job. The templates are just so amazing.",
+    revenue: "30k",
+  },
+]
+
 export default function Testimonials() {
-  return <div>Testimonials</div>
+  return (
+    <StyledTestimonials>
+      <Container>
+        {testimonials.map((testimonial) => (
+          <Testimonial key={testimonial.id} testimonial={testimonial} />
+        ))}
+      </Container>
+    </StyledTestimonials>
+  )
 }
